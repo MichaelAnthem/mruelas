649edfe61576a19917c963f78dcebfb88611bfaa
MichaelRuelas <mruelas@csumb.edu>
2017-09-10 23:03:54 +0000

    adding lab 2

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -4,24 +4,32 @@
     <head>
         <title> 777 Slot Machine </title>
         <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <?php
+        
+        include 'inc/functions.php'
+        
+        ?>
+        
+        
     </head>
     <body>
+        
+        
+        
 
-    <div id="main">
-        <img id='reel1' src='img/cherry.png' alt='cherry' title='Cherry' width='70px' ><img id='reel2' src='img/seven.png' alt='seven' title='Seven' width='70px' ><img id='reel3' src='img/seven.png' alt='seven' title='Seven' width='70px' ><div id='output'><h3> Try Again! </h3></div>        <form>
-          <input type="submit" value="Spin!"/>
+    <div id="main"</div>
+        <?php
+        
+        play();
+        
+        ?>
+           
+        <form>
+         <input type="submit" value="Spin!"/>
         </form>
+         
     </div>
 
-<br /><br />
 
-<footer>
-Image sources: <br>
-http://www.modern-canvas-art.com/ekmps/shops/robboweb1/images/slot-machine-symbols-pop-art-canvas-print-4252-p.jpg
-<br />
-http://www.freeslotmachines.me.uk/
-<br />
-http://img.over-blog-kiwi.com/1/18/84/05/20140812/ob_3ea045_slotmachine-wallpaper-777.jpg
-</footer>
     </body>
 </html>
