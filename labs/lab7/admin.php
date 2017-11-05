3b0752478b7383f9f46279713a3f45f2ef8cfd2c
MichaelRuelas <mruelas@csumb.edu>
2017-11-05 05:33:52 +0000

    adding admin

---
+++ labs/lab7/admin.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+
+//print_r($_SESSION);
+
+if (!isset($_SESSION['username'])) { //if not set, it means that admin hasn't logged in
+    
+    header("Location: index.php"); //redirects users to login page
+    exit;
+    
+}
+
+function authorList(){
+    include '../../dbConnection.php';
+    $conn = getDatabaseConnection();
+    $sql = "SELECT *
+            FROM q_author
+            ORDER BY lastName";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $records;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Admin Section  </title>
+        
+        <script>
+
+            function confirmDelete() {
+                
+                return confirm("Are you sure you want to delete this author?");
+                
+                
+            }            
+            
+        </script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <h1> ADMIN SECTION</h1>
+        <h2> Welcome <?=$_SESSION[adminFullName]?>!</h2>
+
+<br ><br >
+
+<form action="addAuthor.php">
+    <input type="submit" value="Add New Author" />
+</form>
+
+<form action="logout.php">
+    <input type="submit" value="Logout" />
+</form>
+
+
+        <?php 
+        
+        $authors =authorList();
+        
+        foreach($authors as $author) {
+            
+            echo "[<a href='updateAuthor.php?authorId=".$author['authorId']."'>Update</a>] ";
+            //echo "[<a href='deleteAuthor.php?authorId=".$author['authorId']."'>Delete</a>] ";
+            
+            echo "<form style='display:inline' action='deleteAuthor.php' onsubmit='return confirmDelete()'>
+                    <input type='hidden' name='authorId' value='".$author['authorId']."'>
+                    <input type='submit' value='Delete'>
+                  </form>";
+            
+            echo $author['firstName'] . "  " . $author['lastName'] . " " . $author['country'] . "<br>";
+        }
+        
+        
+        ?>
+
+    </body>
+</html>
