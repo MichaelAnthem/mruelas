b3e52bc61e0491edb5a0934b7d82e894894c4ce1
MichaelRuelas <mruelas@csumb.edu>
2017-11-25 05:07:44 +0000

    adding js for homework four

--- hw/hw4/js/functions.js
+++ hw/hw4/js/functions.js
@@ -1,96 +1,41 @@
+function submitAnswers() {
+  var total = 6;
+  var score = 0;
+  
+  
+  // Get User Input
+  var q1 = document.forms["quizForm"] ["q1"].value;
+  var q2 = document.forms["quizForm"] ["q2"].value;
+  var q3 = document.forms["quizForm"] ["q3"].value;
+  var q4 = document.forms["quizForm"] ["q4"].value;
+  var q5 = document.forms["quizForm"] ["q5"].value;
+  var q6 = document.forms["quizForm"] ["q6"].value;
 
+  // Validation 
+  for(i = 1; i <= total;i++) {
+    if(eval('q'+1) == null || eval('q'+1) == '') {
+      alert('You missed question '+ i);
+      return false;
+    }
+  }
+  
+  // Set Correct Answers 
+  var answers = ["c", "a", "a", "b", "a", "a"];
+  
+  // Check Answers
+  for(i = 1; i <= total;i++) {
+    if(eval('q'+1) == answers[i - 1]) {
+      score++;
+    }
+    
+  }
+  
+  //Display Results
+  var results = document.getElementById('results');
+  results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+  alert('You scored '+score+' out of ' +total);
+  
+  return false;
+  
 
-var counter = 0,
-    score = 0;
-
-
-
-var content = [{"name":"Like a Prayer","answer":"madonna"},{"name":"Born This Way","answer":"gaga"},{"name":"So Happy I Could Die","answer":"gaga"},{"name":"Revolver","answer":"madonna"},{"name":"Dancing in Circles","answer":"gaga"},{"name":"Holiday","answer":"madonna"},{"name":"Poker Face","answer":"gaga"},{"name":"Love Game","answer":"gaga"},{"name":"Vogue","answer":"madonna"},{"name":"Edge of Glory","answer":"gaga"}];
-
-
-
-var $name = $('.name'),
-    $generate = $('.generate'),
-    $result = $('.results'),
-    $score = $('.score'),
-    $thanks = $('.thanks'),
-    $options = $('.options');
-
-var trekApp = {};
-
-// the initial state of the quiz:
-// starts off by showing the "name" value in the first item in the data object
-// hides the 'next' button, results, score and 'thanks for playing' html as a default
-
-trekApp.init = function() {
-  var selection = content[counter];
-  type = selection["answer"];
-  $name.html(selection["name"]);
-  $generate.hide();
-  $result.hide();
-  $score.hide();
-  $thanks.hide();
 }
-
-// the function for moving through the quiz
-
-trekApp.generate = function() {
-
-  // if there are still questions remaining, this will show the next one
-  
-  if (counter < content.length) {
-    var selection = content[counter];
-    $name.html(selection["name"]);
-    type = selection["answer"];  
-
-    $result.hide();
-    $score.hide();
-    $name.show();
-    $options.show();
-
-  // if there are no more questions, this will thank the user for playing and give the option to tweet the score
-  
-  } else {
-    $thanks.show().append(" <a href='http://twitter.com/home?status=Take the Star Trek Episode vs. Nail Polish Colour quiz! I scored " + score + " out of " + counter + " http://kerrywall.com/trek-polish' target='_blank'>Tweet your score</a>.");
-  }
-
-  $generate.hide();
-}
-
-// the event handler that determines whether the user's selection was right
-
-$('.choice').click(function(e) {
-  var chosenAnswer = e.target.id;  
-  $result.show();
-  $score.show();
-  $name.hide();
-  $options.hide();
-
-  // Thuis will be setting up "full sentence" values for each type
-  if (type == "gaga") {
-    fullAnswer = "Lady Gaga Song!";
-  } else {
-    fullAnswer = "Madonna Song!";
-  }
-   
-  // This tells the user whether they're right or wrong, and wills add a point to the score if they're right
-
-  if (chosenAnswer == type) {
-    $result.html("<span class='right'>Slay, yo!</span> It's a " + fullAnswer + ".");
-    score++;
-  } else {
-    $result.html("<span class='wrong'>Dammit, Jim!</span> It's a " + fullAnswer + ".");
-  }
-  counter++;
-  $score.html("You're " + score + " for " + counter + ".");
-  $generate.show();
-  
-});
-
-$(document).ready(function() {
-  trekApp.init();
-});
-
-$generate.on('click', function() {
-  trekApp.generate();
-});
