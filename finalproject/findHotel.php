73db12d3f28ae5884501868616218fd2126073ee
MichaelRuelas <mruelas@csumb.edu>
2017-12-07 18:36:16 +0000

    adding final project

--- finalproject/findHotel.php
+++ finalproject/findHotel.php
@@ -8,7 +8,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="js/project.js"></script>
+        
     </head>
     <style>
         body {
